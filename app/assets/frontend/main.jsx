498142c776a26dbd8bde93b70982870a931fe917
Gilberto Villa <ingilniero@gmail.com>
2015-11-03 17:46:48 -0600

    Pass mockTweets to TweetList component in the Home component

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,12 +1,27 @@
 import TweetBox from './components/TweetBox';
 import TweetList from './components/TweetList';
 
+let mockTweets = [
+  {
+    name: 'Gilberto Villa',
+    body: 'My #firstTweet'
+  },
+  {
+    name: 'Eduardo Figarola',
+    body: 'My #firstTweet'
+  },
+  {
+    name: 'Jesus Figueroa',
+    body: 'My #firstTweet'
+  }
+];
+
 class Home extends React.Component {
   render() {
     return (
       <div className='container'>
         <TweetBox />
-        <TweetList />
+        <TweetList tweets={mockTweets}/>
       </div>
     );
   }
