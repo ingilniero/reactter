042303025d8337e08c2fdb7f9c510961b15c861a
Gilberto Villa <ingilniero@gmail.com>
2015-11-03 17:45:55 -0600

    Update TweetList component to get tweets list via its props

--- app/assets/frontend/components/TweetList.jsx
+++ app/assets/frontend/components/TweetList.jsx
@@ -2,11 +2,11 @@ import Tweet from './Tweet';
 
 export default class TweetList extends React.Component {
   render() {
+    let tweets = this.props.tweets.map(tweet => <Tweet {...tweet} key={tweet.name} />);
     return(
       <div>
         <ul className='collection'>
-          <Tweet />
-          <Tweet />
+          {tweets}
         </ul>
       </div>
     );
