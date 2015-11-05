9b8447a7080f15a2c69ddac0666f87f98e62cb0e
Gilberto Villa <ingilniero@gmail.com>
2015-11-05 17:15:47 -0600

    Add profile route/index view

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,10 @@
+class ProfileController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user = current_user
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'profile' => 'profile#index'
+  post 'profile' => 'profile#update'
+
   devise_for :users
   root 'home#index'
 end
