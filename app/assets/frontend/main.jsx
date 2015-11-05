375be7aa94be6bcf790ca4ab3a405a359488e13c
Gilberto Villa <ingilniero@gmail.com>
2015-11-05 16:32:06 -0600

    Add submit functiolity to TweetBox component

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -4,11 +4,27 @@ import TweetList from './components/TweetList';
 let mockTweets = [];
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { tweetsList: mockTweets }
+  }
+
+  addTweet(tweetToAdd) {
+    let newTweetsList = this.state.tweetsList;
+    newTweetsList.unshift({
+      id: Date.now(),
+      name: 'Guest',
+      body: tweetToAdd
+    });
+
+    this.setState({ tweetsList: newTweetsList });
+  }
+
   render() {
     return (
       <div className='container'>
-        <TweetBox />
-        <TweetList tweets={mockTweets}/>
+        <TweetBox sendTweet={this.addTweet.bind(this)} />
+        <TweetList tweets={this.state.tweetsList}/>
       </div>
     );
   }
