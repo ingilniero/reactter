9900b4264c37ef982a2a77c88a0bdcff2611f1a6
Gilberto Villa <ingilniero@gmail.com>
2015-11-05 18:50:36 -0600

    Show user gravatar in list of tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,10 +2,14 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
 
   def as_json(options={})
-    super(methods: [:name])
+    super(methods: [:name, :avatar])
   end
 
   def name
     user.display_name
+  end
+
+  def avatar
+    user.gravatar
   end
 end
