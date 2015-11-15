9571fe4d3b3ea838c2283000ce128fb2895202e3
Gilberto Villa <ingilniero@gmail.com>
2015-11-15 14:59:27 -0600

    Add create action for followers

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -2,4 +2,10 @@ class FollowersController < ApplicationController
   def index
     render json: User.where(["id != ?", current_user.id]).order("random()").all
   end
+
+  def created
+    follower = Follower.create(user_id: params[:user_id], followed_by: current_user.id)
+
+    render json: follower
+  end
 end
