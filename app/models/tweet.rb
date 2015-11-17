13c1f335938c0e502871bc2ee77bd685725f8d9d
Gilberto Villa <ingilniero@gmail.com>
2015-11-16 21:04:00 -0600

    Show stream of tweets only from people I follow

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,16 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
 
+  def self.stream_for(current_user_id)
+    joins(:user)
+    .where(["users.id = :current_user_id or users.id in (
+       select user_id from followers
+       where followed_by = :current_user_id
+      )", { current_user_id: current_user_id }])
+    .order(created_at: :desc)
+    .all
+  end
+
   def as_json(options={})
     super(methods: [:name, :avatar])
   end
