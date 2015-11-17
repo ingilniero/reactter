c79bc3f66ff1b906abaa094d5258874d33b81c2b
Gilberto Villa <ingilniero@gmail.com>
2015-11-16 20:48:16 -0600

    Fix type in create action of followers controller

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -3,7 +3,7 @@ class FollowersController < ApplicationController
     render json: User.where(["id != ?", current_user.id]).order("random()").all
   end
 
-  def created
+  def create
     follower = Follower.create(user_id: params[:user_id], followed_by: current_user.id)
 
     render json: follower
